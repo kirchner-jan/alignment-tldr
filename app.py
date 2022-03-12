ef836467e9977cc68b2b86633334d45fdc4ce079
Jan <jan040793@gmail.com>
2022-03-12 17:41:48 +0100

    good nuff for now

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import transformers, torch
+import requests , html
 
-@st.cache  # 👈 This function will be cached
-def get_model_and_tokenizer(modelpath):
-  device = "cuda:0" if torch.cuda.is_available() else "cpu"
-  tokenizer = transformers.GPT2TokenizerFast.from_pretrained('gpt2' )
-  model = transformers.AutoModelForCausalLM.from_pretrained(modelpath, torch_dtype=torch.float16, low_cpu_mem_usage=True).to(device)
-  return model , tokenizer
+def get_payload(article_text):
+  payload = {"context":html.escape(article_text), "top_p": 0.9, "temp": 0.75}
+  return payload
+def main():
+  st.title('Alignment TL;DR')
+  model_url = st.text_area('Insert address that accepts requests: ')
+  article_text = st.text_area('Insert excessively wordy article here: ')
+  context = '\n\nTL;DR:'
+  tldr_button = st.button('TL;DR!')
+  if tldr_button:
+    if len(article_text) < 1:
+      st.markdown("That's too short.")
+    else: 
+      if len(article_text) > 1000:
+        st.markdown("That's too long, I'll skip some of that.")
+      article_text = article_text[:5000]
+      payload = get_payload(article_text + context)
+      r = requests.post(model_url, json=payload)
+      completion = r.json()["completion"][len(article_text)+5:]
+      completion = completion.split('<|endoftext|>')[0]
+      st.markdown(completion)
 
-def get_message(model , tokenizer , inputSTR , top_p = 1 , temp = 0.9 , max_new_tokens=None):
-  inputs = tokenizer(inputSTR, add_special_tokens=False, return_tensors="pt")["input_ids"].to(device)
-  outputs = model.generate(inputs, no_repeat_ngram_size=5, max_new_tokens=max_new_tokens, top_p=top_p , temp=temp , do_sample=True , typical=0.9)
-  message = tokenizer.decode(outputs[0])
-  return message
-
-st.title('Alignment TL;DR')
-
-modelpath = "/content/drive/MyDrive/g_projects/2022/alignment-hf"
-model , tokenizer = get_model_and_tokenizer(modelpath)
-
-
-article_text = st.text_area('Insert excessively wordy article here: ')
-tldr_button = st.button('TL;DR!')
-if tldr_button:
-  if len(article_text) < 1:
-    st.markdown("That's too short.")
-  else:
-    st.markdown("Sorry, didn't read it either.")
-    st.markdown(article_text)
+if __name__ == '__main__':
+    main()
